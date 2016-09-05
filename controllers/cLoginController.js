e12d5bd5b8e6bdd0545782db513abbcf2d004994
煊宏 <xiangkai.xk@alibaba-inc.com>
2016-09-05 23:57:01 +0800

    OAuth: get accesstoken.

--- controllers/cLoginController.js
+++ controllers/cLoginController.js
@@ -2,7 +2,7 @@
  * Created by xiangkai on 16/9/4.
  */
 var request  = require("request");
-var getAccessTokenURL = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxde4642a10788624f&secret=4e67e578e0318def0512293bff7e1550&code=CODE&grant_type=authorization_code";
+var getAccessTokenURL = "https://api.weixin.qq.com/sns/oauth2/access_token";
 exports.loginPage = function (req, res, next) {
     var loginData = {
         title: '用户登录',
@@ -14,5 +14,19 @@ exports.loginPage = function (req, res, next) {
 exports.weChatCallback = function (req, res, next) {
     res.send("code:" + JSON.stringify(req.query.code));
     var oauthCode = req.query.code;
-    request.get("");
+    var reqParam = {
+        url: getAccessTokenURL,
+        qs: {
+            appid:"wxde4642a10788624f",
+            secret: "4e67e578e0318def0512293bff7e1550",
+            code:oauthCode,
+            grant_type: "authorization_code"
+        }
+    };
+    //body:{\"access_token\":\"1ZIqmH9JV0VAbaFx3aOYRJtlWTSAmsZs5iwaZYD7l0p0qE8T8uc0pzjtC0Tnx3mGdC4aldCbfbY7AS14vwh4ZpWzXVoheUpnfGr1LgYOJmY\",
+    // \"expires_in\":7200,\"refresh_token\":\"bYXfbr4ucAL4KOwiKMD0V28_nRDTQhUKqPowvK_e9otknta4xHf_des2knBMja0gxiuPN-EIHmf2ZkgYWRBrSIFJcNZi5y5ItYEIPKbLdgE\",
+    // \"openid\":\"ox8tuwPgKfc-_ZmY3ues-4TfSrAI\",\"scope\":\"snsapi_userinfo\"}
+    request.get(reqParam,function(error,res,body){
+        console.log(JSON.stringify(body));
+    });
 };
