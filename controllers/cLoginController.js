8807af0656674331b7f7d34ea3cb7730896db7b8
xiangkaiy <xiangkaiy@gmail.com>
2016-09-06 23:54:28 +0800

    Personal info page: part

--- controllers/cLoginController.js
+++ controllers/cLoginController.js
@@ -1,7 +1,7 @@
 /**
  * Created by xiangkai on 16/9/4.
  */
-var request  = require("request");
+var request = require("request");
 var getAccessTokenURL = "https://api.weixin.qq.com/sns/oauth2/access_token";
 exports.loginPage = function (req, res, next) {
     var loginData = {
@@ -16,9 +16,9 @@ exports.weChatCallback = function (req, res, next) {
     var reqParam = {
         url: getAccessTokenURL,
         qs: {
-            appid:"wxde4642a10788624f",
+            appid: "wxde4642a10788624f",
             secret: "4e67e578e0318def0512293bff7e1550",
-            code:oauthCode,
+            code: oauthCode,
             grant_type: "authorization_code"
         }
     };
@@ -27,9 +27,23 @@ exports.weChatCallback = function (req, res, next) {
     // \"expires_in\":7200,\"refresh_token\":\"bYXfbr4ucAL4KOwiKMD0V28_nRDTQhUKqPowvK_e9otknta4xHf_des2knBMja0gxiuPN-EIHmf2ZkgYWRBrSIFJcNZi5y5ItYEIPKbLdgE\",
     // \"openid\":\"ox8tuwPgKfc-_ZmY3ues-4TfSrAI\",\"scope\":\"snsapi_userinfo\"}
 
-    request.get(reqParam,function(error,response,body){
-        console.log(body);
-        var body = JSON.parse(body);
-        res.send("accesstoken:" + body.access_token);
-    });
+    //request.get(reqParam, function (error, response, body) {
+    //    console.log(body);
+    //    var body = JSON.parse(body);
+    //    var personalInfo = {
+    //        avatorUrl: "",
+    //        nickName: "xiangkai",
+    //        gender: "男",
+    //        bindMobilephone: false
+    //    };
+    //    res.render('personal_info', personalInfo);
+    //});
+
+    var personalInfo = {
+        avatorUrl: "",
+        nickName: "xiangkai",
+        gender: "男",
+        bindMobilephone: false
+    };
+    res.render('personal_info', personalInfo);
 };
