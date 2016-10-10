f94b917c44646c31543cf1668bb5a20156c90cc6
xiangkaiy <xiangkaiy@gmail.com>
2016-10-11 00:11:11 +0800

    Add modify pwd dlg.

--- public/javascripts/craftman_setting.js
+++ public/javascripts/craftman_setting.js
@@ -1,8 +1,19 @@
 /**
  * Created by xuanhong on 16/10/9.
  */
-document.addEventListener("DOMContentLoaded",function(){
-   window.logout = function(){
-       window.history.go(-2);
-   };
+document.addEventListener("DOMContentLoaded", function () {
+    window.logout = function () {
+        window.history.go(-2);
+    };
+    var modifyPwdWin = document.getElementById("pop-dlg");
+    window.modifyPwdDlg = function () {
+        modifyPwdWin.style.display = "block";
+    };
+    window.hideModifyDlg = function () {
+        modifyPwdWin.style.display = "none";
+    };
+    var content = document.getElementById("content");
+    content.addEventListener("click", function (e) {
+        e.stopPropagation();
+    });
 });
