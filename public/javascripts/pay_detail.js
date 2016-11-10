a4c5cb5561b734d4b603306d6703272cd042c079
xiangkaiy <xiangkaiy@gmail.com>
2016-11-10 23:33:30 +0800

    Add pop dialog

---
+++ public/javascripts/pay_detail.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var dialog = document.getElementById("dialog");
+    window.payClick = function(){
+        dialog.style.display = "block";
+    }
+});
